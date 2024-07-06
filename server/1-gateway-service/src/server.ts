9d7f5b67aa4f260071a2e41c9948a9cc491f60db
Taras Ivaniuk <tarasivaniuk37@gmail.com>
2024-07-06 19:16:36 +0300

    feat: add gig microservice

--- server/1-gateway-service/src/server.ts
+++ server/1-gateway-service/src/server.ts
@@ -1,121 +1,151 @@
-import { Logger } from 'winston';
+import http from 'http';
+
+import 'express-async-errors';
 import { CustomError, IErrorResponse, winstonLogger } from '@256taras/jobber-shared';
-import { Application, json, NextFunction, urlencoded, Request, Response } from 'express';
+import { Application, Request, Response, json, urlencoded, NextFunction } from 'express';
+import { Logger } from 'winston';
 import cookieSession from 'cookie-session';
+import cors from 'cors';
 import hpp from 'hpp';
 import helmet from 'helmet';
-import cors from 'cors';
 import compression from 'compression';
 import { StatusCodes } from 'http-status-codes';
+import { config } from '@gateway/config';
+import { elasticSearch } from '@gateway/elasticsearch';
+import { appRoutes } from '@gateway/routes';
 import { axiosAuthInstance } from '@gateway/services/api/auth.service';
-
-import * as http from 'http';
-import { config } from './config';
-import { elasticSearch } from './elasticsearch';
-import { appRoutes } from './routes';
+import { axiosBuyerInstance } from '@gateway/services/api/buyer.service';
+import { axiosSellerInstance } from '@gateway/services/api/seller.service';
+import { axiosGigInstance } from '@gateway/services/api/gig.service';
+import { Server } from 'socket.io';
+import { createClient } from 'redis';
+import { createAdapter } from '@socket.io/redis-adapter';
+import { SocketIOAppHandler } from '@gateway/sockets/socket';
 
 const SERVER_PORT = 4000;
+
 const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'apiGatewayServer', 'debug');
+export let socketIO: Server;
 
 export class GatewayServer {
-    private app: Application;
+  private app: Application;
 
-    constructor(app: Application){
-        this.app = app;
-    }
+  constructor(app: Application) {
+    this.app = app;
+  }
 
-    public start(): void {
-        this.securityMiddleware(this.app);
-        this.standardMiddleware(this.app);
-        this.routesMiddleware(this.app);
-        this.startElasticSearch();
-        this.errorHandler(this.app);
-        this.startServer(this.app);
-      }
+  public start(): void {
+    this.securityMiddleware(this.app);
+    this.standardMiddleware(this.app);
+    this.routesMiddleware(this.app);
+    this.startElasticSearch();
+    this.errorHandler(this.app);
+    this.startServer(this.app);
+  }
 
-    private securityMiddleware(app: Application){
-     app.set('trust proxy', 1);
-     app.use(
-        cookieSession({
-          name: 'session',
-          keys: [`${config.SECRET_KEY_ONE}`, `${config.SECRET_KEY_TWO}`],
-          maxAge: 24 * 7 * 3600000,
-          secure: config.NODE_ENV !== 'development',
-          ...(config.NODE_ENV !== 'development' && {
-            sameSite: 'none'
-          })
+  private securityMiddleware(app: Application): void {
+    app.set('trust proxy', 1);
+    app.use(
+      cookieSession({
+        name: 'session',
+        keys: [`${config.SECRET_KEY_ONE}`, `${config.SECRET_KEY_TWO}`],
+        maxAge: 24 * 7 * 3600000,
+        secure: config.NODE_ENV !== 'development',
+        ...(config.NODE_ENV !== 'development' && {
+          sameSite: 'none'
         })
-      );
+      })
+    );
+    app.use(hpp());
+    app.use(helmet());
+    app.use(cors({
+      origin: config.CLIENT_URL,
+      credentials: true,
+      methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH', 'OPTION']
+    }));
 
-     app.use(hpp());
-     app.use(helmet());
-     app.use(cors({
-        origin: '',
-        credentials: true,
-        methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH', 'OPTION']
-     }));
+    app.use((req: Request, _res: Response, next: NextFunction) => {
+      if (req.session?.jwt) {
+        axiosAuthInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
+        axiosBuyerInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
+        axiosSellerInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
+        axiosGigInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
+      }
+      next();
+    });
+  }
 
-      app.use((req: Request, _res: Response, next: NextFunction) => {
-        if (req.session?.jwt) {
-          axiosAuthInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
-        }
-        next();
+  private standardMiddleware(app: Application): void {
+    app.use(compression());
+    app.use(json({ limit: '200mb' }));
+    app.use(urlencoded({ extended: true, limit: '200mb' }));
+  }
+
+  private routesMiddleware(app: Application): void {
+    appRoutes(app);
+  }
+
+  private startElasticSearch(): void {
+    elasticSearch.checkConnection();
+  }
+
+  private errorHandler(app: Application): void {
+    app.use('*', (req: Request, res: Response, next: NextFunction) => {
+      const fullUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
+      log.log('error', `${fullUrl} endpoint does not exist.`, '');
+      res.status(StatusCodes.NOT_FOUND).json({ message: 'The endpoint called does not exist.'});
+      next();
+    });
+
+    app.use((error: IErrorResponse, _req: Request, res: Response, next: NextFunction) => {
+      if (error instanceof CustomError) {
+        log.log('error', `GatewayService ${error.comingFrom}:`, error);
+        res.status(error.statusCode).json(error.serializeErrors());
+      }
+
+      next();
+    });
+  }
+
+  private async startServer(app: Application): Promise<void> {
+    try {
+      const httpServer: http.Server = new http.Server(app);
+      const socketIO: Server = await this.createSocketIO(httpServer);
+      await this.startHttpServer(httpServer);
+      this.socketIOConnections(socketIO);
+    } catch (error) {
+      log.log('error', 'GatewayService startServer() error method:', error);
+    }
+  }
+
+  private async createSocketIO(httpServer: http.Server): Promise<Server> {
+    const io: Server = new Server(httpServer, {
+      cors: {
+        origin: `${config.CLIENT_URL}`,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+      }
+    });
+    const pubClient = createClient({ url: config.REDIS_HOST });
+    const subClient = pubClient.duplicate();
+    await Promise.all([pubClient.connect(), subClient.connect()]);
+    io.adapter(createAdapter(pubClient, subClient));
+    socketIO = io;
+    return io;
+  }
+
+  private async startHttpServer(httpServer: http.Server): Promise<void> {
+    try {
+      log.info(`Gateway server has started with process id ${process.pid}`);
+      httpServer.listen(SERVER_PORT, () => {
+        log.info(`Gateway server running on port ${SERVER_PORT}`);
       });
+    } catch (error) {
+      log.log('error', 'GatewayService startServer() error method:', error);
     }
+  }
 
-    private standardMiddleware(app: Application){
-        app.use(compression());
-        app.use(json({limit: '200mb'}));
-        app.use(urlencoded({extended: true, limit: '200m'}));
-
-    }
-
-    private routesMiddleware(app: Application):void{
-       appRoutes(app);
-
-    }
-
-    private startElasticSearch():void{
-
-        elasticSearch.checkConnection();
-    }
-
-    private errorHandler(app: Application): void {
-        app.use('*', (req: Request, res: Response, next: NextFunction) => {
-          const fullUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
-          log.log('error', `${fullUrl} endpoint does not exist.`, '');
-          res.status(StatusCodes.NOT_FOUND).json({ message: 'The endpoint called does not exist.'});
-          next();
-        });
-
-        app.use((error: IErrorResponse, _req: Request, res: Response, next: NextFunction) => {
-          if (error instanceof CustomError) {
-            log.log('error', `GatewayService ${error.comingFrom}:`, error);
-            res.status(error.statusCode).json(error.serializeErrors());
-          }
-
-          next();
-        });
-      }
-
-      private async startServer(app: Application): Promise<void> {
-        try {
-          const httpServer: http.Server = new http.Server(app);
-
-          await this.startHttpServer(httpServer);
-        } catch (error) {
-          log.log('error', 'GatewayService startServer() error method:', error);
-        }
-      }
-
-      private async startHttpServer(httpServer: http.Server): Promise<void> {
-        try {
-          log.info(`Gateway server has started with process id ${process.pid}`);
-          httpServer.listen(SERVER_PORT, () => {
-            log.info(`Gateway server running on port ${SERVER_PORT}`);
-          });
-        } catch (error) {
-          log.log('error', 'GatewayService startServer() error method:', error);
-        }
-      }
+  private socketIOConnections(io: Server): void {
+    const socketIoApp = new SocketIOAppHandler(io);
+    socketIoApp.listen();
+  }
 }
